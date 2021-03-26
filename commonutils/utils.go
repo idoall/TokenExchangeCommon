1ae96e2b230f99b9beda59513af7685046f80c75
idoall <lion.888@gmail.com>
2021-03-26 22:18:37 +0800

    修复FloatFromStringDontRound方法的一个bug

--- commonutils/utils.go
+++ commonutils/utils.go
@@ -633,7 +633,9 @@ func FloatFromStringDontRound(num float64, exp int) (result float64, err error) 
 	n := strconv.FormatFloat(num, 'f', -1, 32)
 	newn := strings.Split(n, ".")
 
-	if len(newn[1]) >= exp {
+	if len(newn) == 0 {
+		result, err = strconv.ParseFloat(newn[0], 64)
+	} else if len(newn[1]) >= exp {
 		result, err = strconv.ParseFloat(newn[0]+"."+newn[1][:exp], 64)
 	} else {
 		result, err = strconv.ParseFloat(newn[0]+"."+newn[1], 64)
